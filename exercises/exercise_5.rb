280587a5a7d065a18b41f9a3235ec4c68ea3953c
George-was-here <george.burt@gmail.com>
2022-06-02 20:45:23 +0000

    Adding exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total annual revenue of all stores", Store.sum(:annual_revenue)
+puts "Average annual revenue", Store.average(:annual_revenue)
+puts "Number of stores with 1M + revenue", Store.where("annual_revenue > 1000000").count
